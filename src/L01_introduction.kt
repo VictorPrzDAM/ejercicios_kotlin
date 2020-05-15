4b86c4e98e7a43ea5e7e3c6ea9a8fc6246b90bed
VictorPrzDAM <netwebsailor@gmail.com>
2020-05-15 17:39:03 +0200

    trabajo de funciones 20200510 1504

--- src/L01_introduction.kt
+++ src/L01_introduction.kt
@@ -1,8 +1,14 @@
 //fun main(args: Array<String>) { // en versiones anteriores al 1.3
 fun main() {
     //El primer HolaMundo en Kotlin
-    print("Hello, Word!")
+    println("Hello, Word!")
     //Ejemplo de función
+    print(imprimeMensaje("hola soy el método \"imprimeMensaje\" "))
+    // Función con parámetro opcional
+    // print(imprimirMensajeConPrefijo("hola soy el método \"imprimirMensajeConPrefijo\"", "¿"))
+    print(imprimirMensajeConPrefijo("hola soy el método \"imprimirMensajeConPrefijo\""))
+    //Función que retorna un valor
+    println("La suma es : "+suma(1,2))
 }
 //Ejemplo de una función:
 /**
@@ -15,6 +21,16 @@ fun main() {
 fun imprimeMensaje(mensaje: String): Unit {
     println("Mensaje desde Método imprime mensaje")
 }
-fun imprimirMensajeConPrefijo(mensaje : String , prefix: String = "info"){
+/**
+String = "info" --> permite un parámetro opcional
+ */
+fun imprimirMensajeConPrefijo(mensaje: String, prefix: String = "info"):Unit {
+    println("[$prefix] $mensaje")
+}
 
+/**
+ * : Int --> indica el retorno de un entero.
+ */
+fun suma(x: Int, y: Int): Int {
+    return x + y
 }
