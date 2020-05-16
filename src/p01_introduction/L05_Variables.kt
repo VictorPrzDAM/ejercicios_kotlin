f3b73884459f7df6e7bef0f8cba068c66d92d50e
VictorPrzDAM <netwebsailor@gmail.com>
2020-05-16 14:44:55 +0200

    commit nueva rama 2

--- src/p01_introduction/L05_Variables.kt
+++ src/p01_introduction/L05_Variables.kt
@@ -2,8 +2,13 @@ package p01_introduction
 
 // Variables:
 
-fun main (){
+fun main() {
 
+    /*
+   1 Declares a mutable variable and initializing it.
+   2 Declares an immutable variable and initializing it.
+   3 Declares an immutable variable and initializing it without specifying the type. The compiler infers the type Int.
+     */
     var a: String = "initial"  // 1
     println(a)
     val b: Int = 1             // 2
