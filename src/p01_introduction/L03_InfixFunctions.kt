82157d9f59ef45d737e2b172ed79005cc3b3e2bf
VictorPrzDAM <netwebsailor@gmail.com>
2020-05-16 14:09:44 +0200

    OperationFuncions

--- src/p01_introduction/L03_InfixFunctions.kt
+++ src/p01_introduction/L03_InfixFunctions.kt
@@ -28,20 +28,18 @@ fun main() {
      */
 
     infix fun Int.times(str: String) = str.repeat(this)        // 1
-    println( 2 times "Bye ")                                    // 2
+    println( 2 times "Bye ")                                      // 2
 
-    val pair = "Ferrari" to "Katrina"                          // 3
+    val pair = "Ferrari" to "Katrina"                             // 3
     println(pair)
 
-    infix fun String.onto(other: String) = Pair(this, other)   // 4
+    infix fun String.onto(other: String) = Pair(this, other)      // 4
     val myPair = "McLaren" onto "Lucas"
     println(myPair)
 
     val sophia = Person("Sophia")
     val claudia = Person("Claudia")
-    sophia likes claudia                                       // 5
-
-
+    sophia likes claudia                                          // 5
 
 
     infix fun Int.shl(x: Int): Int {
@@ -53,8 +51,6 @@ fun main() {
 
 // is the same as
     1.shl(2)
-
-
 }
 
 class Person(val name: String) {
