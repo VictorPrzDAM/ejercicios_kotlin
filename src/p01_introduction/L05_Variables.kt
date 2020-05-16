ef871b68f7a0e52eb226e1f6252e9d9cac055714
VictorPrzDAM <netwebsailor@gmail.com>
2020-05-16 14:38:46 +0200

    un commit desde intelliJ

---
+++ src/p01_introduction/L05_Variables.kt
@@ -0,0 +1,13 @@
+package p01_introduction
+
+// Variables:
+
+fun main (){
+
+    var a: String = "initial"  // 1
+    println(a)
+    val b: Int = 1             // 2
+    val c = 3                  // 3
+
+
+}
